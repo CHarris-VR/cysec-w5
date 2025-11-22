e1d3303f7144d1ac46a8cf0bc5c7cf7066821bd5
CHarris-VR <charrisagent@gmail.com>
2025-11-22 13:18:22 -0500

    added critical user input

--- login_analyzer.py
+++ login_analyzer.py
@@ -1,4 +1,6 @@
 
+# user input
+
 username = input("Enter username: ")
 failed_logins = int(input("Enter number of failed login attempts: "))
 
@@ -6,3 +8,16 @@ if failed_logins > 5:
     print(f"[!] ALERT: {username} has {failed_logins} failed logins!")
 else:
     print(f"User {username} appears normal.")
+
+# adding critical user
+
+critical_user = input("Is this a privileged user? (yes/no): ")
+
+if failed_logins > 5 and critical_user.lower() == "yes":
+    print("[*] CRITICAL ALERT: Privileged account under attack!")
+elif failed_logins > 5:
+    print("[!] ALERT: Multiple failed logins detected.")
+elif failed_logins > 0:
+    print("[-] Notice: Some failed attempts observed.")
+else:
+    print("[+] No failed logins recorded.")
