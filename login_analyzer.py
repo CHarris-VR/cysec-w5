31a32aa69418928d9827dd27892f09290214f9eb
CHarris-VR <charrisagent@gmail.com>
2025-11-22 14:26:48 -0500

    updated code for the challenge

--- login_analyzer.py
+++ login_analyzer.py
@@ -1,31 +1,45 @@
 
+
+# Get analyst input
+
+analyst = input("Enter Analyst name: ")
+
 # user input
 
 username = input("Enter username: ")
 failed_logins = int(input("Enter number of failed login attempts: "))
 
-if failed_logins > 5:
-    print(f"[!] ALERT: {username} has {failed_logins} failed logins!")
-else:
-    print(f"User {username} appears normal.")
-
-# adding critical user
-
 critical_user = input("Is this a privileged user? (yes/no): ")
 
-if failed_logins > 5 and critical_user.lower() == "yes":
-    print("[*] CRITICAL ALERT: Privileged account under attack!")
-elif failed_logins > 5:
-    print("[!] ALERT: Multiple failed logins detected.")
-elif failed_logins > 0:
-    print("[-] Notice: Some failed attempts observed.")
-else:
-    print("[+] No failed logins recorded.")
+# adusting reporting
 
-# adding reporting
+print("\n=== Login Attempt Analyzer ===")
+print(f"Analyzer: {analyst}")
 
 print("\n=== Login Attempt Summary ===")
 print(f"Username: {username}")
 print(f"Failed Attempts: {failed_logins}")
 print(f"Privileged Account: {critical_user}")
 
+# Critial User Logic
+
+if failed_logins > 5 and critical_user.lower() == "yes":
+    print("Risk Level: HIGH")
+    print("[*] CRITICAL ALERT: Privileged account under attack!")
+elif failed_logins > 5:
+    print("Risk Level: MEDIUM")
+    print("[!] ALERT: Multiple failed logins detected.")
+elif 1 <= failed_logins <= 5:
+    print("Risk Level: LOW")
+    print("[!] Notic: Some Failed attempts observed.")
+elif failed_logins == 0:
+    print("Risk Level: Informational")
+    print("[+] Info: Normal login activity observed.")
+
+# Import and print date time
+
+from datetime import datetime
+
+time = datetime.now()
+print("\n=== Report Generated ===")
+print(time)
